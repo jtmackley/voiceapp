3d5adc34c3c60af795f53b6b4ab9b921b515a941
James Mackley <jtmackley@gmail.com>
2015-06-21 20:06:17 +0100

    Added ui grid

--- test.py
+++ test.py
@@ -1,26 +1,65 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
+def GetCategories():
+    categories=[]
+    category = Categories()
+    category.word="General"
+    category.words.append("Yes")
+    category.words.append("No")
+    category.words.append("Please")
+    category.words.append("Thankyou")
+    categories.append(category)
 
-class Example(QtGui.QMainWindow):
+    category = Categories()
+    category.word="Food"
+    category.words.append("More")
+    category.words.append("Finished")
+    category.words.append("Hot")
+    category.words.append("Cold")
+    categories.append(category)
+
+    category = Categories()
+    category.word="Bathroom"
+    category.words.append("Option1")
+    category.words.append("Option2")
+    categories.append(category)
+
+    category = Categories()
+    category.word="Sleep"
+    category.words.append("Tired")
+    category.words.append("Upstairs")
+    category.words.append("Downstairs")
+    categories.append(category)
+
+    return categories
+
+class Categories():
+    def __init__(self):
+        self.word=""
+        self.words=[]
+
+class TalkBoard(QtGui.QWidget):
     
     def __init__(self):
-        super(Example, self).__init__()
+        super(TalkBoard, self).__init__()
         
         self.initUI()
         
     def initUI(self):      
 
-        btn1 = QtGui.QPushButton("Button 1", self)
-        btn1.move(30, 50)
+        # Load Categories
+        categories=GetCategories()
 
-        btn2 = QtGui.QPushButton("Button 2", self)
-        btn2.move(150, 50)
-      
-        btn1.clicked.connect(self.buttonClicked)            
-        btn2.clicked.connect(self.buttonClicked)
-        
-        self.statusBar()
+        grid = QtGui.QGridLayout()
+        self.setLayout(grid)
+
+        for row, category in enumerate(categories):
+            button = QtGui.QPushButton(category.word)
+            button.clicked.connect(self.buttonClicked)            
+            grid.addWidget(button, row,1)
+            
+
         
         self.setGeometry(300, 300, 290, 150)
         self.setWindowTitle('Event sender')
@@ -29,12 +68,12 @@ class Example(QtGui.QMainWindow):
     def buttonClicked(self):
       
         sender = self.sender()
-        self.statusBar().showMessage(sender.text() + ' was pressed')
+        print sender.text() + ' was pressed'
         
 def main():
     
     app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    ex = TalkBoard()
     sys.exit(app.exec_())
 
 
